2a4ddc99a7e5e11eb39796a42619c24908a8612f
sv2 <sv2@slana.tech>
2019-12-03 22:02:30 -0800

    Initial

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-// Vuetify CSS
-import 'vuetify/dist/vuetify.min.css';
 
 // Dashblocks CSS
 import 'dashblocks/dist/dashblocks.css';
 import './quasar';
 
 Vue.config.productionTip = false;
-
-Vue.use(Vuetify);
 
 new Vue({
   router,
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/');
   });
 });
