9b56fc1958c5bc4549d2b21451992d22ab5d393d
sv2 <sv2@slana.tech>
2020-06-08 17:01:13 -0700

    forms

---
+++ src/components/tables/hitstable.vue
@@ -0,0 +1,237 @@
+<template>
+  <q-table
+    title="Hits"
+    dense
+    flat
+    :data="hits"
+    :columns="columnsList"
+    :pagination.sync="pagination"
+    :rows-per-page-options="[10, 20, 50, 100, 200]"
+    row-key="name"
+    separator="cell"
+    class="ub-hits-table"
+    @request="onRequest"
+  >
+    <template v-slot:top-right="props">
+      <q-btn flat round dense icon="archive" size="md" />
+      <q-btn flat round dense :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'" @click="props.toggleFullscreen" class="q-ml-md" size="md" />
+    </template>
+
+    <template v-slot:header="props">
+      <q-tr :props="props">
+        <q-th v-for="col in props.cols" :key="col.name" :props="props" class="ub-th">
+          {{ col.label }}
+        </q-th>
+      </q-tr>
+    </template>
+
+    <template v-slot:body="props">
+      <tr v-if="hasHitColumn" class="ub-hits-tr" @click="onRowClick(props.row)" @dblclick="onRowDblClick(props.row)">
+        <td class="ub-hits-td-time">
+          {{ formatTsColumn(props.row) }}
+        </td>
+        <td class="ub-hits-td-hit">
+          <code class="ub-code" v-html="formatJsonColumn(props.row)"></code>
+        </td>
+      </tr>
+      <tr v-else class="ub-hits-tr" @click="onRowClick(props.row)" @dblclick="onRowDblClick(props.row)">
+        <td v-for="col in props.cols" :key="col.name">
+          {{ formatColumn(col, props.row) }}
+        </td>
+      </tr>
+    </template>
+  </q-table>
+</template>
+<script>
+import pathOr from 'ramda/src/pathOr';
+
+// Specialized table to show collection of JSON documents
+export default {
+  name: 'HitsTable',
+  props: {
+    hits: {
+      type: Array,
+      default: () => []
+    },
+    tsPath: {
+      type: Array,
+      default: () => ['_source', '@timestamp']
+    },
+    tsLabel: {
+      type: String,
+      default: '@timestamp'
+    },
+    displayFields: {
+      type: Object,
+      default: () => {}
+    },
+    displayFieldsRemovePrefix: {
+      type: String,
+      default: null
+    },
+    page: {
+      type: Number,
+      default: 1
+    },
+    rowsPerPage: {
+      type: Number,
+      default: 10
+    },
+    totalHits: {
+      type: Number,
+      default: 0
+    },
+    sortBy: {
+      type: String,
+      default: ''
+    },
+    descending: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data: function() {
+    return {
+      pagination: {
+        page: this.page,
+        rowsPerPage: this.rowsPerPage,
+        rowsNumber: this.totalHits,
+        sortBy: this.sortBy,
+        descending: this.descending
+      },
+      tsColumn: {
+        name: this.tsLabel,
+        required: true,
+        label: this.tsLabel,
+        align: 'left',
+        format: this.formatTsColumn,
+        sortable: true,
+        style: 'vertical-align: top;'
+      },
+      hitColumn: {
+        name: 'hit',
+        required: true,
+        label: 'Hit',
+        align: 'left',
+        sortable: false
+      },
+      columns: [],
+      items: [],
+      allColumnsList: [],
+      thStops: [25, 50, 100],
+      thStopsWidth: [100, 140, 160]
+    };
+  },
+  computed: {
+    hasHitColumn: function() {
+      return Object.keys(this.displayFields).length <= 0;
+    },
+    columnsList: function() {
+      let allColumnsList = [this.tsColumn];
+      if (this.hasHitColumn) {
+        allColumnsList.push(this.hitColumn);
+      } else {
+        for (let dc of Object.keys(this.displayFields)) {
+          let fn = dc;
+          if (this.displayFieldsRemovePrefix && fn.startsWith(this.displayFieldsRemovePrefix)) {
+            fn = fn.replace(this.displayFieldsRemovePrefix, '');
+          }
+          allColumnsList.push({
+            name: fn,
+            required: true,
+            label: fn,
+            field: dc,
+            align: 'left',
+            sortable: true
+          });
+        }
+      }
+      return allColumnsList;
+    }
+  },
+  mounted: function() {},
+  watch: {
+    totalHits: function(val) {
+      this.pagination.rowsNumber = val;
+    },
+    page: function(val) {
+      this.pagination.page = val;
+    },
+    rowsPerPage: function(val) {
+      this.pagination.rowsPerPage = val;
+    },
+    sortBy: function(val) {
+      this.pagination.sortBy = val;
+    },
+    descending: function(val) {
+      this.pagination.descending = val;
+    }
+  },
+  methods: {
+    getThStyle(col) {
+      let len = col.label.length;
+      let idx = this.thStops.findIndex(x => x > len);
+      return idx !== -1 ? `min-width:${this.thStopsWidth[idx]}px;` : 'min-width:250px;';
+    },
+
+    formatColumn(col, row) {
+      if (col.label === this.tsLabel) {
+        return this.formatTsColumn(row);
+      }
+      let path = pathOr(null, [col.field], this.displayFields);
+      if (!path) {
+        return '';
+      }
+      return pathOr('', path, row);
+    },
+
+    formatTsColumn(row) {
+      return pathOr(null, this.tsPath, row);
+    },
+
+    formatJsonColumn(row) {
+      let code = JSON.stringify(row, (key, value) => {
+        if (typeof value === 'string') {
+          return `<span class='ub-v-s'>${value}</span>`;
+        } else if (typeof value === 'number') {
+          return `<span class='ub-v-n'>${value}</span>`;
+        }
+        return value;
+      });
+      return code;
+    },
+
+    onRowClick(row) {
+      this.$emit('rowClick', row);
+    },
+
+    onRowDblClick(row) {
+      this.$emit('rowDblClick', row);
+    },
+
+    onRequest(props) {
+      console.log(`Got onRequest: ${JSON.stringify(props)}`);
+      this.$emit('tableRequest', props);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.ub-hits-table {
+  width: 100%;
+
+  .ub-hits-tr {
+    font-family: 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace;
+  }
+
+  .ub-hits-td-time {
+    vertical-align: top;
+  }
+
+  .ub-hits-td-hit {
+    vertical-align: top;
+    white-space: normal;
+    word-break: break-all;
+  }
+}
+</style>
